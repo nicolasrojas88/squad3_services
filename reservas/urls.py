99ac144a1cbb62d8e6c9c9237673c5322ce8a4ef
Ariel Chocobar <arielteacher4@gmail.com>
2023-05-30 18:52:21 -0300

    se registra mod ReservaServicio

--- reservas/urls.py
+++ reservas/urls.py
@@ -5,6 +5,6 @@ from reservas import views
 urlpatterns =[
     path('alta_emp/', views.alta_empleado_vista, name='alta_empleado'),
     path('servicios', views.servicios_vista, name="servicios"),
-    path("empleado/activar/<id_legajo>/", views.empleado_activa, name="empleado_activa"),
-    path("empleado/desactivar/<id_legajo>/", views.empleado_desactiva, name="empleado_activa")
+    path("empleado/activar/<int:id_legajo>/", views.empleado_activa, name="empleado_activa"),
+    path("empleado/desactivar/<int:id_legajo>/", views.empleado_desactiva, name="empleado_activa")
 ]
