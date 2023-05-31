0f441cc4e97b8ebfc3b737dd49ebfddfedaff9a2
reproevo <100879652+reproevo@users.noreply.github.com>
2023-05-31 02:23:52 -0300

    correccion para cumplir criterios de aceptacion

--- reservas/urls.py
+++ reservas/urls.py
@@ -7,5 +7,5 @@ urlpatterns =[
     path('servicios', views.servicios_vista, name="servicios"),
     path("empleado/activar/<id_legajo>/", views.empleado_activa, name="empleado_activa"),
     path("empleado/desactivar/<id_legajo>/", views.empleado_desactiva, name="empleado_activa"),
-    path('empleados/<int:empleado_id>', views.actualizar_empleado_vista, name="actualizar_empleado"),
+    path('empleados/modificar/<int:empleado_id>', views.actualizar_empleado_vista, name="actualizar_empleado"),
 ]
